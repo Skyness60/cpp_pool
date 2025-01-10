bb7ae27cd495994b82c3bca0b09f6417fb067a45
sami.perron60@gmail.com <sami.perron60@gmail.com>
2025-01-11 00:40:59 +0100

    dog.hpp init

--- module_04/ex00/Dog.hpp
+++ module_04/ex00/Dog.hpp
@@ -1,3 +1,15 @@
 #ifndef DOG_HPP
 #define DOG_HPP
+#include <iostream>
+#include <string>
+#include "Animal.hpp"
+
+class Dog : public Animal {
+	public:
+		Dog();
+		Dog(const Dog &other);
+		Dog &operator=(const Dog &copy);
+		virtual ~Dog();
+		virtual void makeSound() const;
+};
 #endif // !DOG_HPP
