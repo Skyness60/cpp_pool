8b988c2c678b42761f060d0f1564b8b0c3b5b863
Skyness60 <sami.perron60@gmail.com>
2025-01-13 16:12:02 +0100

    refactor: update Makefile to use AAnimal class and modify main function for testing

--- module_04/ex02/main.cpp
+++ module_04/ex02/main.cpp
@@ -7,3 +7,21 @@
 #include "Cat.hpp"
 #include "Brain.hpp"
 
+
+int	main()
+{
+	// Dosen't work, because Animal is abstract
+	//AAnimal	ani = AAnimal();
+	//ani.makeSound();
+
+	AAnimal	*cat = new Cat();
+	cat->makeSound();
+
+	// Dosen't work, because Animal is abstract
+	//AAnimal	dog = Dog();
+	//dog.makeSound();
+
+	delete cat;
+
+	return 0;
+}
