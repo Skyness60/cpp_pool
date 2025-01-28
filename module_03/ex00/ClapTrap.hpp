84e02c6577f325b72c46ca318bd5edd26ab52574
Skyness60 <sami.perron60@gmail.com>
2025-01-28 14:07:38 +0100

    refactor: initialize ClapTrap attributes in constructor and add color coding for debug output

--- module_03/ex00/ClapTrap.hpp
+++ module_03/ex00/ClapTrap.hpp
@@ -1,7 +1,14 @@
 #ifndef CLAPTRAP_HPP
 #define CLAPTRAP_HPP
 #include <iostream>
-
+#define RESET  "\033[0m"
+#define BOLD_RED  "\033[1m\033[31m"      /* Bold Red */
+#define BOLD_GREEN  "\033[1m\033[32m"    /* Bold Green */
+#define BOLD_YELLOW  "\033[1m\033[33m"   /* Bold Yellow */
+#define BOLD_BLUE  "\033[1m\033[34m"     /* Bold Blue */
+#define BOLD_MAGENTA  "\033[1m\033[35m"  /* Bold Magenta */
+#define BOLD_CYAN  "\033[1m\033[36m"     /* Bold Cyan */
+#define BOLD_WHITE  "\033[1m\033[37m"    /* Bold White */
 class ClapTrap
 {
 private:
