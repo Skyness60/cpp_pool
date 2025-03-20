ef23fa1c8eea1d168e1d8b36e74ebafa2805016b
sami.perron60@gmail.com <sami.perron60@gmail.com>
2025-03-20 14:46:03 +0100

    refactor: update ShrubberyCreationForm to inherit from AForm and add copy constructor

--- module_05/ex02/ShrubberyCreationForm.cpp
+++ module_05/ex02/ShrubberyCreationForm.cpp
@@ -1,5 +1,19 @@
 #include "ShrubberyCreationForm.hpp"
 
-ShrubberyCreationForm::ShrubberyCreationForm(std::string target) : target(target) {
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target) : AForm(target, 145, 137) {
     std::cout << BOLD_GREEN "constructor ShrubberyCreationForm " << this->name << " created" RESET << std::endl;
 }
+
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm const &copy) : AForm(copy)
+{
+    std::cout << BOLD_GREEN "constructor copy ShrubberyCreationForm " << this->name << " created" RESET << std::endl;
+}
+
+// ShrubberyCreationForm &ShrubberyCreationForm::operator=(const ShrubberyCreationForm copy)
+// {
+//     std::cout << BOLD_GREEN "assignation operator ShrubberyCreationForm " << this->name << " created" RESET << std::endl;
+// 	if (this not_eq &copy)
+// 	{
+// 		this->name = copy.name;
+// 	}
+// }
