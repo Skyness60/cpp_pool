bb6012626a5c2e3f8ea68693eb04678fb1082618
sami.perron60@gmail.com <sami.perron60@gmail.com>
2025-03-21 18:43:03 +0100

    refactor: implement PresidentialPardonForm and RobotomyRequestForm classes with constructors, destructors, and execute methods

--- module_05/ex02/include/PresidentialPardonForm.hpp
+++ module_05/ex02/include/PresidentialPardonForm.hpp
@@ -2,5 +2,17 @@
 #define PresidentialPardonForm_HPP
 #include "AForm.hpp"
 
+class Bureaucrat;
+
+class PresidentialPardonForm : public AForm 
+{
+    public:
+		PresidentialPardonForm(std::string target);
+		PresidentialPardonForm(PresidentialPardonForm const &copy);
+        virtual ~PresidentialPardonForm(void);
+        PresidentialPardonForm   &operator=(PresidentialPardonForm const &copy);
+
+        virtual void    execute(Bureaucrat const &executor) const;
+};
 
 #endif
---
+++ module_05/ex02/src/PresidentialPardonForm.cpp
@@ -0,0 +1,34 @@
+#include "PresidentialPardonForm.hpp"
+#include "Bureaucrat.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm(std::string target) : AForm("Shrubbery", target, 145, 137) {
+    std::cout << BOLD_GREEN "constructor PresidentialPardonForm " << this->name << " created" RESET << std::endl;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(PresidentialPardonForm const &copy) : AForm(copy)
+{
+    std::cout << BOLD_GREEN "constructor copy PresidentialPardonForm " << this->name << " created" RESET << std::endl;
+}
+
+PresidentialPardonForm &PresidentialPardonForm::operator=(const PresidentialPardonForm &copy)
+{
+    std::cout << BOLD_GREEN "assignation operator PresidentialPardonForm " << this->name << " created" RESET << std::endl;
+	if (this not_eq &copy)
+		this->target = copy.target;
+	return *this;
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+	std::cout << BOLD_RED "destructor PresidentialPardonForm " << this->name << " destroyed" RESET << std::endl;
+}
+
+void PresidentialPardonForm::execute(Bureaucrat const &executor) const
+{
+	if (!this->getSigned())
+		return (std::cout << "Form is not signed" << std::endl, void());
+	if (executor.getGrade() > this->getGradeExec()) {
+		throw AForm::GradeTooLowException();
+	}
+	std::cout << BOLD_BLUE << this->target << " has been pardoned by Zafod Beeblebrox" RESET << std::endl;
+}
