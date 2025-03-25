c37b0cbba78d7547db6e323270ba7e10bf263151
Skyness60 <sami.perron60@gmail.com>
2025-03-25 17:47:17 +0100

    refactor: update char output formatting and improve error handling in ScalarConverter

--- module_06/ex00/src/ScalarConverter.cpp
+++ module_06/ex00/src/ScalarConverter.cpp
@@ -16,7 +16,7 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &copy)
 ScalarConverter::~ScalarConverter() {}
 
 static void		charConvert(std::string param) {
-	std::cout << BOLD_CYAN << "char: " << param[0] << RESET << std::endl;
+	std::cout << BOLD_CYAN << "char: '" << param[0] << "'" << RESET << std::endl;
 	std::cout << BOLD_GREEN << "int: " << static_cast<int>(param[0]) << RESET << std::endl;
 	std::cout << BOLD_YELLOW << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(param[0]) << "f" << RESET << std::endl;
 	std::cout << BOLD_MAGENTA << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(param[0]) << RESET << std::endl;
@@ -30,7 +30,7 @@ static void		intConvert(std::string param) {
 	else if (n < 32 || n == 127)
 		std::cout << BOLD_RED << "char: non-displayable" << RESET << std::endl;
 	else
-		std::cout << BOLD_CYAN << "char: " << static_cast<char>(n) << RESET << std::endl;
+		std::cout << BOLD_CYAN << "char: '" << static_cast<char>(n) << "'" << RESET << std::endl;
 	std::cout << BOLD_GREEN << "int: " << n << RESET << std::endl;
 	std::cout << BOLD_YELLOW << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(n) << "f" << RESET << std::endl;
 	std::cout << BOLD_MAGENTA << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(n) << RESET << std::endl;
@@ -44,7 +44,7 @@ static void		floatConvert(std::string param) {
 	else if (n < 32 || n == 127)
 		std::cout << BOLD_RED << "char: non-displayable" << RESET << std::endl;
 	else
-		std::cout << BOLD_CYAN << "char: " << static_cast<char>(n) << RESET << std::endl;
+		std::cout << BOLD_CYAN << "char: '" << static_cast<char>(n) << "'" << RESET << std::endl;
 	std::cout << BOLD_GREEN << "int: " << static_cast<int>(n) << RESET << std::endl;
 	std::cout << BOLD_YELLOW << "float: " << std::fixed << std::setprecision(1) << n << "f" << RESET << std::endl;
 	std::cout << BOLD_MAGENTA << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(n) << RESET << std::endl;
@@ -58,7 +58,7 @@ static void		doubleConvert(std::string param) {
 	else if (n < 32 || n == 127)
 		std::cout << BOLD_RED << "char: non-displayable" << RESET << std::endl;
 	else
-		std::cout << BOLD_CYAN << "char: " << static_cast<char>(n) << RESET << std::endl;
+		std::cout << BOLD_CYAN << "char: '" << static_cast<char>(n) << "'" << RESET << std::endl;
 	std::cout << BOLD_GREEN << "int: " << static_cast<int>(n) << RESET << std::endl;
 	std::cout << BOLD_YELLOW << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(n) << "f" << RESET << std::endl;
 	std::cout << BOLD_MAGENTA << "double: " << std::fixed << std::setprecision(1) << n << RESET << std::endl;
@@ -88,8 +88,8 @@ static void		nanConvert() {
 static void		error() {
 	std::cout << BOLD_RED << "char: impossible" << RESET << std::endl;
 	std::cout << BOLD_RED << "int: impossible" << RESET << std::endl;
-	std::cout << BOLD_YELLOW << "float: " << 0 << RESET << std::endl;
-	std::cout << BOLD_MAGENTA << "double: " << 0 << RESET << std::endl;
+	std::cout << BOLD_RED << "float: impossible" << RESET << std::endl;
+	std::cout << BOLD_RED << "double: impossible" << RESET << std::endl;
 }
 
 static void		checkInput(std::string param) {
